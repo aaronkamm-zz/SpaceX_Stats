4009c883a50dee58d3ccf3cf35371b6afa7e66dd
aaron <aaronakamm@gmail.com>
2019-02-23 23:00:39 +0200

    Created schema for "Launch Type"

--- schema.js
+++ schema.js
@@ -1,10 +1,15 @@
-const { GraphQLObjectType, GraphQLInt} = require('graphql');
+const {GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLBoolean} = require('graphql');
 
 //Launch Type 
 const Launch = new GraphQLObjectType({
     name: 'Launch',
     fields: () => ({
         flight_number: {type: GraphQLInt},
-        
+        mission_name: {type: GraphQLString},
+        launch_year: {type: GraphQLString},
+        launch_date_local: {type: GraphQLString},
+        launch_success: {type: GraphQLBoolean}
     })
 });
+
+//Rocket Type

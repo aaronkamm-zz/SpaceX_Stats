7d64a01ce96127a2bcf5ddf458785494d6e2d726
aaron <aaronakamm@gmail.com>
2019-03-10 14:04:45 +0200

    Made some styling changes to RocketItem

--- client/src/components/RocketItem.js
+++ client/src/components/RocketItem.js
@@ -10,9 +10,9 @@ export default function RocketItem({rocketInfo: {mission_name, rocket: {rocket_n
     <div className = "card mb-3">
         <h4 className = "card-header"><span className = "font-italic">{mission_name}</span> Rocket Info</h4>
         <div className = "card-body">
-            <h4>Rocket Name: {rocket_name}</h4> 
-            <h4>Manufacturer: {manufacturer}</h4>
-            <h4>Nationality: {nationality}</h4>
+            <h4><span className="font-italic">Rocket Name</span>: {rocket_name}</h4> 
+            <h4><span className="font-italic">Manufacturer</span>: {manufacturer}</h4>
+            <h4><span className="font-italic">Nationality</span>: {nationality}</h4>
         </div>
     </div>
   )
